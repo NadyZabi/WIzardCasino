c00bfea6bbd458017e7974058b0579109a23c53b
Nadezhda <zaborskaiang@gmail.com>
2020-12-08 10:32:51 +0100

    Rewrite table with jinja2 package. Add data.csv

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, url_for
+import pandas as pd
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -15,9 +18,28 @@ def about():
 def chart():
     return render_template("chart.html")
 
+# Table parametres
+
+headings = ('Time','Black','Zero','Red')
+"""
+data = pd.read_csv("data.csv", sep=',', header=None, index_col=False)
+
+#for row in data.iterrows():
+#    print(row)
+
+print(data)
+"""
+data = (
+    ('00:38:25','','0',''),
+    ('00:37:38','15','',''),
+    ('00:36:48','','','32'),
+    ('00:35:56','','','18'),
+    ('00:34:27','','','7')
+)
+
 @app.route('/table')
 def table():
-    return render_template("table.html")
+    return render_template("table.html", headings=headings, data=data)
 
 if __name__=="__main__":
     app.run(debug=True)
