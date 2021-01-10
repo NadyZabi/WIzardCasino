2cf900b8a62b55d6607168ba68a0f474c6db34ba
Nadezhda <zaborskaiang@gmail.com>
2021-01-10 21:27:52 +0100

    Query params for statistics

--- app.py
+++ app.py
@@ -12,12 +12,16 @@ data = pd.read_csv('data.csv', sep=";")
 cat = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5,
        24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
 
+red_cat = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34 ,36]
+black_cat =[2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33 , 35]
+zero_cat=[0]
+
 # models.py - M in MVC
 
 tmp = pd.read_csv('data.csv', sep=";")
 
 def read_again(file_name: str = "data.csv"):
-    """ read the content of the file """ 
+    """ read the content of the file """
     return pd.read_csv(file_name, sep=";")
 
 # end of model py
@@ -25,12 +29,12 @@ def read_again(file_name: str = "data.csv"):
 
 
 # should be in controller.py:
-def render_index() -> Any:
-    """ prepare data for user view """
-    return render_template("index.html")
+#def render_index() -> Any:
+#    """ prepare data for user view """
+#    return render_template("index.html")
 
-def get_stats(refresh: str):
-    """ 
+def get_stats(refresh: str, colors):
+    """
     Get stats and refresh data source if needed
     :param refresh: bool if the source of data should be read again for this request
     :return: render_template function with all params
@@ -40,23 +44,25 @@ def get_stats(refresh: str):
     if refresh=="1":
         print("Yassss!")
         tmp = read_again()
-    num = []
-    for col in headings[1:]:
-        arr = column_to_int(tmp,col)
+
+    num =[]
+    for color in colors:
+
+        arr = column_to_int(tmp,color)
         num +=arr
+        miss_key = (set(cat) - set(count_frequency(num).keys()))
+        miss_val = { i : 0 for i in miss_key }
 
-    miss_key = (set(cat) - set(count_frequency(num).keys()))
-    miss_val = { i : 0 for i in miss_key }
+        labels = list(count_frequency(num).keys())+list(miss_key)
+        val = list(count_frequency(num).values())+list(miss_val.values())
 
-    labels = list(count_frequency(num).keys())+list(miss_key)
-    val = list(count_frequency(num).values())+list(miss_val.values())
+
     return headings, tmp, labels, val
-# end of controller
 
 
 @app.route('/')
 def index():
-    return render_index
+    return render_template("index.html")
 
 
 # Visual endpoint
@@ -95,9 +101,11 @@ def stat():
     # val = list(count_frequency(num).values())+list(miss_val.values())
     #
     # return render_template("stat.html",headings=headings, data=tmp,labels=labels, val=val)
-    params = request.args
-    refresh = "lol" if "refresh" not in params.keys() else params["refresh"]
-    headings, data, labels, val = get_stats(refresh)
+    params = request.args.to_dict(flat=False)
+    refresh = '0' if "refresh" not in params.keys() else params["refresh"]
+    colors = ['Red', 'Black', 'Zero'] if "color" not in params.keys() else params["color"]
+    print(colors)
+    headings, data, labels, val = get_stats(refresh, colors)
     return render_template("stat.html", headings=headings, data=data, labels=labels, val=val)
 
 if __name__=="__main__":
